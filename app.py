149c2d7675acdad907f6a123a7516c0a67d106e6
Charlie Westaway <charleswestaway@gmail.com>
2021-03-08 14:02:40 -0500

    Added Basic Image Handling
    - Installed Pillow for basic image handling.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from PIL import Image
+
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -31,13 +33,28 @@ def post_something():
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
     if param:
         return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
+            "Message": f"Welcome {param} to our awesome platform!!",
             # Add this option to distinct the POST request
             "METHOD" : "POST"
         })
     else:
         return jsonify({
             "ERROR": "no name found, please send a name."
+        })
+
+@app.route('/strip/', methods=['POST'])
+def analyze_strip():
+    file = request.files['strip']
+    # Read the image via file.stream
+    img = Image.open(file.stream)
+    if img:
+        return jsonify({
+            'msg': 'success', 
+            'size': [img.width, img.height]
+        })
+    else:
+        return jsonify({
+            "ERROR": "no strip found, please send a strip."
         })
 
 # A welcome message to test our server
